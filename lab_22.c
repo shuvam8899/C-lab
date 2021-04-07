0d2b8e053e3fc1d00b4ffcc4e80b85770d91ef28
shuvam8899 <81801473+shuvam8899@users.noreply.github.com>
2021-04-07 13:01:33 +0530

    Program to print the Fibonacci sequence using recursion

---
+++ lab_22.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int printFibonacci(int n){
+	static int n1 = 0, n2 = 1, n3;
+		if (n>0){
+		n3 = n1 + n2;
+		n1 = n2;
+		n2 = n3;
+		printf("%d ", n3);
+		printFibonacci(n-1);
+	}
+}
+	int main(){
+		int n;
+		printf("enter the number of elements:\n");
+		scanf("%d", &n);
+		printf("fibonacci series:\n");
+		printf("%d %d ", 0, 1);
+		printFibonacci(n-2) ;
+	return 0;
+}
